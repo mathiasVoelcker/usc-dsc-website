ce2ebbb3ac79a323bba5e183f7d9fe3e0aa91154
Ziyi Wu <ziyiwu@usc.edu>
2020-01-13 23:04:53 -0800

    event updates

--- js/index.js
+++ js/index.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
 		// add form-specific values into the data
 		formData.formDataNameOrder = JSON.stringify(fields);
 		formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-		formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+		formData.formGoogleSendEmail = form.dataset.email || "uscdeveloperstudentclub@gmail.com"; // no email by default
 
 		console.log(formData);
 		return formData;
